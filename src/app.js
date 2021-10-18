d67df24a3e873fe0b8a3d078406fd3ce1b3b7dbf
Inês Lopes <70814170+ines-codes@users.noreply.github.com>
2021-10-18 13:27:34 +0100

    Forecast API

--- src/app.js
+++ src/app.js
@@ -32,7 +32,9 @@ let currentTime = document.querySelector("#date");
 currentTime.innerHTML = formatDate();
 
 /// Display the forecast
-function displayForecast() {
+
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast-display");
   let forecastHTML = `<div class="row">`;
   let days = ["thu", "fri", "sat"];
@@ -88,7 +90,7 @@ function showTemperature(response) {
     response.data.wind.speed
   );
 
-  /// For the forecast
+  /// Display the forecast
   displayForecast();
   ///end
 
@@ -121,6 +123,15 @@ form.addEventListener("submit", handleSubmit);
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
+/// Receiving coords for the forecast
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "836945bb1ae780c68d086d693cfcb666";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
+
 // Changing today's icon according to the description
 
 function showIcon(response) {
@@ -128,6 +139,10 @@ function showIcon(response) {
 
   iconElement.setAttribute("src", `icons/${response.data.weather[0].icon}.png`);
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  /// Getting lat and lon for the forecast
+  getForecast(response.data.coord);
+  ///end
 }
 
 // Changing from Fahrenheit to Celsius and Vice Versa
